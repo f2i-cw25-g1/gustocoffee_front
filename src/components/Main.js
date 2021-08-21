5626e444bcd816631ed518b38c5a65dfe9f19323
Alexisreau97 <alexis.reaupro@gmail.com>
2021-08-21 11:33:42 +0200

    modification texte

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,7 +11,7 @@ function Main() {
       <section className="bienvenue">
         <div className="bienvenue_text">
           <h2 className="section_title">
-            Bienvenue au Gusto Coffee, votre espace de coworking
+            Bienvenue au Gusto Coffee, votre espace de coworking Gare du Nord
           </h2>
           <p className="section_text">
             Gusto Coffee élargit son offre et vous propose désormais des espaces de travail. Vous êtes à la recherche d'un lieu convivial afin de travailler seul ou en équipe ? Découvrez notre grande salle ainsi que nos salons.
@@ -59,7 +59,7 @@ function Main() {
       <p className="subsection_title">Nos Cafés</p>
 
       <Products
-        categorie = "/api/categories/1"
+        categorie="/api/categories/1"
       />
 
       <div className="center">
@@ -71,7 +71,7 @@ function Main() {
       <p className="subsection_title">Nos Produits dérivés</p>
 
       <Products
-        categorie = "/api/categories/6"
+        categorie="/api/categories/6"
       />
 
       <div className="center">
