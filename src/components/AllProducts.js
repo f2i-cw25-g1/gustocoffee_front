66e2c07876a361855831c1a1ab4305c87f6193ba
Eric <eric.ciccotti@gmail.com>
2021-08-23 09:34:28 +0200

    problem en cours de résolution

--- src/components/AllProducts.js
+++ src/components/AllProducts.js
@@ -1,89 +1,99 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
 import "./css/Carousel.css";
 import axios from "axios";
-import ReactDOM from 'react-dom';
 
-import Product from "./Product";
 
-function AllProducts() {
-/*
-    const [products, setProducts] = useState([]);//products = résultat du axios get, set products peut être modifié, ne change rien, usestate appelle use effect
 
-    let allProducts = [];
-    let categories = [];
+const AllProducts = () => {
+    const productsRef = useRef([]);
+    const categoriesRef = useRef([]);
+    const documentRef = useRef(null);
 
-    const getData = async () => {
-        await axios.get(`/api/produits`)//récupération des produits
-        .then((response) => {
-            allProducts = response.data["hydra:member"];
-            axios.get(`/api/categories`)//récupération des catégories après la récupération des produits
-            .then((response) => {
-                categories = response.data["hydra:member"] ;
-                categories.forEach((categorie)=>{
-                    categorie.produits.forEach((produit, index)=>{//pour chaque produit de chaque catégorie, on remplace l'id du produit stocké dans le tableau catégories par l'objet produit lié à cet id
-                        categorie.produits[index] = allProducts.find(element => element.id == produit.split("/api/produits/")[1]);
-                    });
-                });
-                console.log(categories);
-            }, (error) => {});
-        }, (error) => {});
-    };
-*/
+    documentRef.current = document.getElementById("loadAllProducts");
 
-    const [products, setProducts] = useState([]);
-    const [categories, setCategories] = useState([]);
-    let responseProducts=[];
-    let responseCategories=[];
-
-    const getProducts = async () => {
-        const { data } = await axios.get(`/api/produits`);
-        responseProducts = data["hydra:member"];
-        setProducts(data["hydra:member"]);
-        getCategories(responseProducts);
-    };
-
-    const getCategories = async (responseProducts) => {
-        const { data } = await axios.get(`/api/categories`);
-        responseCategories = data["hydra:member"];
-        console.log(data["hydra:member"]);
-        document.getElementById("loadAllProducts").style.display = "none";
-       
-        responseCategories.forEach((categorie)=>{
-            categorie.produits.forEach((produit, index)=>{//pour chaque produit de chaque catégorie, on remplace l'id du produit stocké dans le tableau catégories par l'objet produit lié à cet id
-                categorie.produits[index] = responseProducts.find(element => element.id == produit.split("/api/produits/")[1]);
-            });
-        });
-        
-        setCategories(responseCategories);
-    };
 
     useEffect(() => {
-    getProducts();
-    }, []);
+        const ourRequest = axios.CancelToken.source();
+
+        let doc = document.getElementById("loadAllProducts");
+
+
+        const getAllProducts = async () => {
+            try {
+                const [requestProducts, requestCategory] = await Promise.all([
+                    axios.get(`/api/produits`, { cancelToken: ourRequest.token }),
+                    axios.get(`/api/categories`, { cancelToken: ourRequest.token })
+                ]);
+
+                productsRef.current = await requestProducts.data["hydra:member"];
+                categoriesRef.current = await requestCategory.data["hydra:member"];
+
+
+                // for (const [index, categorie] of categoriesRef.current.entries()) {
+                //     for (const [index, p] of categorie.produits.entries()) {
+                //         categoriesRef.current[index] = p.produits[index] = productsRef.current.find(element => element['@id'] === p);
+                //     }
+                // }
+
+
+                // console.log('CATGEROEI REF 1', categoriesRef.current);
+
+                let responseProducts = productsRef.current;
+                let responseCategories = categoriesRef.current;
+
+                categoriesRef.current = responseCategories.current.forEach((categorie) => {
+                    categorie.produits.forEach((produit, index) => {//pour chaque produit de chaque catégorie, on remplace l'id du produit stocké dans le tableau catégories par l'objet produit lié à cet id
+                        categorie.produits[index] = responseProducts.find(element => element.id === produit.split("/api/produits/")[1]);
+                    });
+                });
+
+                doc.style.display = "none";
+
+                console.log('CATGEROEI REF 2', categoriesRef.current);
+
+
+
+
+            } catch (error) {
+                console.log('Il ya eu un problème, ou la requete a été interrompue')
+            }
+        }
+
+        getAllProducts();
+
+
+
+        return () => {
+            console.log('composant démonté')
+            ourRequest.cancel('component demonté')
+        }
+    }, [])
+
+
 
 
     return (
         <div>
             <div className="load" id="loadAllProducts"></div>
-            {categories.map((categorie) =>{
-                return(
+            {categoriesRef.current && categoriesRef.current.map((categorie) => {
+                return (
                     <div className="categorie" key={categorie.nom}>
                         <div className="subsection_title">
-                        {categorie.nom}
+                            {categorie.nom}
                         </div>
-                        <div className="items">
-                        {categorie.produits.map((produit) =>{
-                            return(
-                                <Product
-                                key={produit.id}
-                                id={produit.id}
-                                nom={produit.nom}
-                                image={produit.image}
-                                />
-                            );
-                        })}
-                        </div>
+                        {/* <div className="items">
+                            {categoriesRef.current.produits.map((produit) => {
+                                return (
+                                    <Product
+                                        key={produit.id}
+                                        id={produit.id}
+                                        nom={produit.nom}
+                                        image={produit.image}
+                                    />
+                                );
+                            })}
+                        </div> */}
                     </div>
                 );
             })}
