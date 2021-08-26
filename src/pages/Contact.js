0307c4e2b3256e5b4d1cf5290b1353d9e9580433
Alexisreau97 <alexis.reaupro@gmail.com>
2021-08-26 16:34:13 +0200

    marge infos contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,10 +5,12 @@ function Contact() {
     <main>
       <p className="subsection_title">Contact</p>
       <Form />
-      <p>Journées d'ouverture : 7 jours sur 7</p>
-      <p>Horaires d'ouverture : de 7h à 22h</p>
-      <p>Adresse : 25 Rue de Dunkerke</p>
-      <p>Numéro de téléphone : <a className="tellink" href="tel:0165389423">01.65.38.94.23</a></p>
+      <div className="contact_infos">
+        <p>Journées d'ouverture : 7 jours sur 7</p>
+        <p>Horaires d'ouverture : de 7h à 22h</p>
+        <p>Adresse : 25 Rue de Dunkerke</p>
+        <p>Numéro de téléphone : <a className="tellink" href="tel:0165389423">01.65.38.94.23</a></p>
+      </div>
     </main>
   );
 }
