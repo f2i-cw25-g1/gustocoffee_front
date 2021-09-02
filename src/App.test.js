ad90ffc27614e64b30b001054d0fe0e93de5f885
Alexisreau97 <alexis.reaupro@gmail.com>
2021-09-02 18:11:45 +0200

    ajout test intégration

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('test texte bienvenue sur page accueil', () => {
+test('test titre 1', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Bienvenue/i);
+  const linkElement = screen.getByText(/Bienvenue au Gusto Coffee, votre espace de coworking Gare du Nord/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test('test2', () => {
+test('test titre 2', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Bienvenue au Gusto Coffee/i);
+  const linkElement = screen.getByText(/Découvrez notre salon de 120 places/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('test titre 3', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Découvrez nos salons de 4 à 6 places/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('test Bouton "Réserver une place"', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Réserver une place/i);
   expect(linkElement).toBeInTheDocument();
 });
